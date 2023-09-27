58a3ba9493393d8cd8c6d58e83a4821b53b2d163
Diego <diego.cast047@gmail.com>
2023-09-26 22:58:39 -0600

    db connected

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+//hacerlo en otro archivo en caso de requerirlo
+const db = require('./config/database');
 
-app.listen(3000, () => {
+app.get('/', async (req, res, next) => {
+  //checar la conexion y funcionalidad de la bd, agregar datos a la bd
+  return res.status(200).json({ message: 'Hola mundo' });
+});
+
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000');
 });
